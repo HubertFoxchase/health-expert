9321f5aeee01c6cb6f765b091fea36cd0fffa8c9
HubertFoxchase <Hubert.Foxchase@gmail.com>
2015-07-13 21:07:38 +0100

    updates

--- s/js/controllers.js
+++ s/js/controllers.js
@@ -1,13 +1,17 @@
 angular.module("controllers", []).
-	controller("AppCtrl", ['$scope', '$rootScope', '$mdSidenav', '$api', '$timeout', function($scope, $rootScope, $mdSidenav, $api, $timeout){
+	controller("AppCtrl", ['$scope', '$rootScope', '$mdSidenav', '$api', '$timeout', '$location', function($scope, $rootScope, $mdSidenav, $api, $timeout, $location){
 		
 		var _api;
+
+		$scope.refresh = false;
 		
 		$scope.toggleSidenav = function(menuId) {
 			    $mdSidenav(menuId).toggle();
 		};
 		
-		$scope.refresh = false;
+		$scope.back = function(){
+			$location.path('/grid');			
+		}
 		
 		var poll = function(){
 			$timeout(function(){
@@ -25,6 +29,8 @@ angular.module("controllers", []).
 		var timer = 0;
 		var _api = $api.get();
 
+		$rootScope.showBackBtn = false;
+		
 		var loadSessions = function(){
 			_api.session.listActive().execute(function(resp){
 				$scope.sessions = resp.items;
@@ -37,12 +43,27 @@ angular.module("controllers", []).
 			loadSessions();
 		})
 		
-		$scope.showDetail = function($event, itemData) {
+		$scope.showDetail = function(itemData, $event) {
 			window.clearInterval(timer);
 			timer = 0;
 			$rootScope.item = itemData;
 	    	$location.path("/session/" + itemData.id);
 	    };
+
+	    $scope.selected = [];
+	    
+	    $scope.toggle = function (item, list, $event) {
+	        
+	    	var idx = list.indexOf(item);
+	        if (idx > -1) list.splice(idx, 1);
+	        else list.push(item);
+
+	        $event.stopPropagation();
+	    };
+	    
+	    $scope.exists = function (item, list) {
+	        return list.indexOf(item) > -1;
+	    };	    
 	    
 		if($rootScope.gridItems){
 			$scope.sessions	= $rootScope.gridItems;
@@ -51,7 +72,6 @@ angular.module("controllers", []).
 			$scope.sessions = null;
 			loadSessions();
 		}
-			//timer = window.setInterval(loadSessions, 5000);
 
 		document.getElementById("left").style.visibility = "visible";
 		document.getElementById("main").style.visibility = "visible";
@@ -62,6 +82,8 @@ angular.module("controllers", []).
 		
 		var _api = $api.get();
 
+		$rootScope.showBackBtn = true;
+		
 		var loadSession = function(){
 			_api.session.get({id:$routeParams.id}).execute(function(resp){
 				$scope.item = resp;
